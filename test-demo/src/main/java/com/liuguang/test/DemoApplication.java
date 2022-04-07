bd3b5468438441fb2806931d5c9ba0b7af57cb8e
wanghaoran <1427965161@qq.com>
2022-04-07 15:50:54 +0800

    初始化项目

---
+++ test-demo/src/main/java/com/liuguang/test/DemoApplication.java
@@ -0,0 +1,11 @@
+package com.liuguang.test;
+
+/**
+ * @className: DemoApplication
+ * @description:
+ * @author: whr
+ * @date: 2022/4/7 15:50
+ * @version: 1.0.0
+ **/
+public class DemoApplication {
+}
